6c44d46a791bcb81e6afe0255344d10c82db1abd
budfy <serdyuk.ro@gmail.com>
2021-04-19 17:48:56 +0300

    Slider arrows fix

--- src/js/01__main.js
+++ src/js/01__main.js
@@ -140,28 +140,36 @@ $(document).ready(function () {
 		slidesToShow: 4,
 		slidesToScroll: 1,
 		responsive: [{
-			breakpoint: 1600,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 1
+				breakpoint: 1600,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			}, {
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					arrows: false,
+					dots: true
+				}
+			}, {
+				breakpoint: 639,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false,
+					dots: true
+				}
 			}
-		}, {
-			breakpoint: 1200,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 1,
-				arrows: false,
-				dots: true,
-			}
-		}, {
-			breakpoint: 639,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				arrows: false,
-				dots: true,
-			}
-		}]
+		]
 	})
 
 	var $food = $('.food-slider');
